b7269734a112f8b066524a283dcef3ea7a0173ce
arijit mahapatra <arimahan.16@gmail.com>
2018-04-08 14:13:24 +0530

    Added two new components

---
+++ ui/src/app/unit/unit.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UnitComponent } from './unit/unit.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UnitComponent]
+})
+export class UnitModule { }
